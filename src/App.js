c97ae569dc9d1e71aaa425be7819968f6080c29a
Anesu Kabuku <125449219+kabukuanesu@users.noreply.github.com>
2024-03-25 08:15:15 +0200

    Lets  build the client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import "./App.css";
 // import Login from "./components/Login";
-import Signup from "./components/Signup";
+// import Signup from "./components/Signup";
 
+import Client from "./components/Client";
 // import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <Client />
     </div>
   );
 }
